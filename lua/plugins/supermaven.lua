acf8b0a014ed78966d63f8e91892c95d2f11c893
Philip Brocoum <philip.brocoum@gmail.com>
2024-07-14 08:21:24 -0700

    Trying Codeium

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,6 +1,8 @@
+-- Trying Codeium
 return {
   {
     "supermaven-inc/supermaven-nvim",
+    enabled = false,
     config = function()
       require("supermaven-nvim").setup({ disable_inline_completion = true })
     end,
@@ -8,7 +10,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      table.insert(opts.sources, { name = "supermaven" })
+      -- table.insert(opts.sources, { name = "supermaven" })
       -- https://github.com/LazyVim/LazyVim/discussions/2032#discussioncomment-8031990
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
