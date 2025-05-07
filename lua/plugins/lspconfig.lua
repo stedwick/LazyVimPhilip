f766dd57853d5714804d57e9c701ba71602ef1ca
Philip Brocoum <philip.brocoum@gmail.com>
2025-05-07 15:18:27 -0700

    feat: add Mason and Mason LSPConfig for improved language server management
    - Integrated Mason.nvim for automatic installation of language servers.
    - Configured Mason LSPConfig for automatic server setup.
    - Enhanced nvim-lspconfig with capabilities and on_attach functions for lua_ls, tsserver, and rust_analyzer.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,8 +1,66 @@
 return {
   {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    cmd = "Mason",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+        "rust-analyzer",
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      automatic_installation = true,
+    },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     opts = {
       inlay_hints = { enabled = false },
     },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      
+      -- Setup common capabilities
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      
+      -- Setup common on_attach function
+      local on_attach = function(client, bufnr)
+        -- Add your keymaps and other on_attach configurations here
+      end
+      
+      -- Configure individual servers
+      lspconfig.lua_ls.setup({
+        cmd = { "/opt/homebrew/bin/lua-language-server" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+      
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+      
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+    end,
   },
 }
